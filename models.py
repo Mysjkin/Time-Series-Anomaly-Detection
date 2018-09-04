4b3a4b9e360c77ff63ce5030755013ed44952963
mysjkin <iarogolo@gmail.com>
2018-09-04 22:18:46 +0200

    Model.

--- models.py
+++ models.py
@@ -1,2 +1,44 @@
+# Model architecture taken from: https://blog.keras.io/building-autoencoders-in-keras.html 
 
+
+from keras.layers import Input, LSTM, RepeatVector, Dense 
+from keras.models import Model 
+from keras import optimizers
+
+def get_model(input_dim, latent_dim, segment_size, optimizer, learning_rate=1e-5, loss='mse'):
+    """ Creates an lstm-based autoencoder.
+
+    Args:
+        :param input_dim:
+        :param latent_dim:
+        :param segment_size:
+        :param optimizer:
+        :param learning_rate:
+        :param loss:
+
+    Returns: a keras model.
+    """
+    # Define input tensor.         
+    inputs = Input(shape=(segment_size, input_dim))         
+    # Define encoder with latent_dim encoding capacity.         
+    encoder = LSTM(latent_dim, return_state=True)         
+    # Run encoder on input tensor.         
+    _, encoded, cell_state = encoder(inputs)         
+    # Output from the encoder is a context vector which encodes         
+    # information about a segment of data points.         
+    # In this model the context vector is used as input to the         
+    # decoder.         
+    # Additionally, this model uses the encoders final states         
+    # to initialize the decoder's initial states.        
+    # Repeat encoded information segment_size times such that all         
+    # decoder LSTM units receives it as input.         
+    repeat = RepeatVector(segment_size)         
+    # Repeat encoded information.         
+    decoder_input = repeat(encoded)         
+    # Run decoder with context vector as input and encoders final states.         
+    decoded = LSTM(latent_dim, return_sequences=True)(decoder_input, initial_state=[encoded, cell_state])         
+    # Convert the dimension of decoded values to the input dimension.         
+    decoded_dense = Dense(input_dim)(decoded)         
+    # Define model input and output         
+    return Model(inputs, decoded_dense)
 
--- training.py
+++ training.py
@@ -3,7 +3,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./data/100.csv', nrows=2500)
+import models
+from sklearn.preprocessing import MinMaxScaler
 
-plt.plot(range(0,2100), df[0:2100]['v5'], color='blue')
-plt.show()
+df = pd.read_csv('./data/100.csv', nrows=2500)
+df = df[['v5','mlii']]
+
+df = df.as_matrix().astype('float32')
+scaler = MinMaxScaler()
+df = scaler.fit_transform(df)
+
+model = models.get_model(2,4,5,'Adam')
+
+
+
+
